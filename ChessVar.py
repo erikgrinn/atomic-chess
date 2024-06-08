084d89a6f05ed99877d171e5c98b547b2795c5ba
Erik Grinn <grinn.erik@gmail.com>
2024-06-07 23:47:05 -0400

    added unicode art to ChessPiece class, modified area to avoid duplicates on edge of board (which would causes issues when capturing a King)

--- ChessVar.py
+++ ChessVar.py
@@ -6,10 +6,11 @@
 
 class ChessPiece:
     """ class of chess piece defining type, position, and color """
-    def __init__(self, type, position, color):
+    def __init__(self, type, position, color, art):
         self._type = type
         self._position = position
         self._color = color
+        self._art = art
 
     def get_type(self):
         """ returns piece type """
@@ -22,6 +23,10 @@ class ChessPiece:
     def get_color(self):
         """ returns piece color """
         return self._color
+
+    def get_art(self):
+        """ returns art of chess piece for printing"""
+        return self._art
 
     def set_position(self, position):
         """ sets piece to new position """
@@ -45,34 +50,34 @@ class ChessVar:
         colors = ['white', 'black']
         for col in columns:
             # pawns
-            board[col + rows[1]] = ChessPiece(pieces[0], col + rows[1], colors[0])
-            board[col + rows[6]] = ChessPiece(pieces[0], col + rows[6], colors[1])
+            board[col + rows[1]] = ChessPiece(pieces[0], col + rows[1], colors[0], '♙')
+            board[col + rows[6]] = ChessPiece(pieces[0], col + rows[6], colors[1], '♟')
 
         # rooks
-        board[columns[0] + rows[0]] = ChessPiece(pieces[3], columns[0] + rows[0], colors[0])
-        board[columns[7] + rows[0]] = ChessPiece(pieces[3], columns[7] + rows[0], colors[0])
-        board[columns[0] + rows[7]] = ChessPiece(pieces[3], columns[0] + rows[7], colors[1])
-        board[columns[7] + rows[7]] = ChessPiece(pieces[3], columns[7] + rows[7], colors[1])
+        board[columns[0] + rows[0]] = ChessPiece(pieces[3], columns[0] + rows[0], colors[0], '♖')
+        board[columns[7] + rows[0]] = ChessPiece(pieces[3], columns[7] + rows[0], colors[0], '♖')
+        board[columns[0] + rows[7]] = ChessPiece(pieces[3], columns[0] + rows[7], colors[1], '♜')
+        board[columns[7] + rows[7]] = ChessPiece(pieces[3], columns[7] + rows[7], colors[1], '♜')
 
         # knights
-        board[columns[1] + rows[0]] = ChessPiece(pieces[1], columns[1] + rows[0], colors[0])
-        board[columns[6] + rows[0]] = ChessPiece(pieces[1], columns[6] + rows[0], colors[0])
-        board[columns[1] + rows[7]] = ChessPiece(pieces[1], columns[1] + rows[7], colors[1])
-        board[columns[6] + rows[7]] = ChessPiece(pieces[1], columns[6] + rows[7], colors[1])
+        board[columns[1] + rows[0]] = ChessPiece(pieces[1], columns[1] + rows[0], colors[0], '♘')
+        board[columns[6] + rows[0]] = ChessPiece(pieces[1], columns[6] + rows[0], colors[0], '♘')
+        board[columns[1] + rows[7]] = ChessPiece(pieces[1], columns[1] + rows[7], colors[1], '♞')
+        board[columns[6] + rows[7]] = ChessPiece(pieces[1], columns[6] + rows[7], colors[1], '♞')
 
         # bishops
-        board[columns[2] + rows[0]] = ChessPiece(pieces[2], columns[2] + rows[0], colors[0])
-        board[columns[5] + rows[0]] = ChessPiece(pieces[2], columns[5] + rows[0], colors[0])
-        board[columns[2] + rows[7]] = ChessPiece(pieces[2], columns[2] + rows[7], colors[1])
-        board[columns[5] + rows[7]] = ChessPiece(pieces[2], columns[5] + rows[7], colors[1])
+        board[columns[2] + rows[0]] = ChessPiece(pieces[2], columns[2] + rows[0], colors[0], '♗')
+        board[columns[5] + rows[0]] = ChessPiece(pieces[2], columns[5] + rows[0], colors[0], '♗')
+        board[columns[2] + rows[7]] = ChessPiece(pieces[2], columns[2] + rows[7], colors[1], '♝')
+        board[columns[5] + rows[7]] = ChessPiece(pieces[2], columns[5] + rows[7], colors[1], '♝')
 
         # queens
-        board[columns[3] + rows[0]] = ChessPiece(pieces[4], columns[3] + rows[0], colors[0])
-        board[columns[3] + rows[7]] = ChessPiece(pieces[4], columns[3] + rows[7], colors[1])
+        board[columns[3] + rows[0]] = ChessPiece(pieces[4], columns[3] + rows[0], colors[0], '♕')
+        board[columns[3] + rows[7]] = ChessPiece(pieces[4], columns[3] + rows[7], colors[1], '♛')
 
         # kings
-        board[columns[4] + rows[0]] = ChessPiece(pieces[5], columns[4] + rows[0], colors[0])
-        board[columns[4] + rows[7]] = ChessPiece(pieces[5], columns[4] + rows[7], colors[1])
+        board[columns[4] + rows[0]] = ChessPiece(pieces[5], columns[4] + rows[0], colors[0], '♔')
+        board[columns[4] + rows[7]] = ChessPiece(pieces[5], columns[4] + rows[7], colors[1], '♚')
 
         return board
 
@@ -117,8 +122,10 @@ class ChessVar:
                     return False
 
                 area = self.explosion_area(finish)
+
                 king_count = 0
                 for pos in area:
+                    print(pos)
                     if pos in self._board:
                         if self._board[pos].get_type() == 'king':
                             king_count += 1
@@ -161,22 +168,22 @@ class ChessVar:
         if 0 <= center_letter_idx < 7:
             right_col = center_letter_idx + 1
         else:
-            right_col = center_letter_idx
+            right_col = None
 
         if center_letter_idx > 0:
             left_col = center_letter_idx - 1
         else:
-            left_col = center_letter_idx
+            left_col = None
 
         if 1 <= int(center[1]) < 8:
             upper_row = int(center[1]) + 1
         else:
-            upper_row = int(center[1])
+            upper_row = None
 
         if int(center[1]) > 1:
             lower_row = int(center[1]) - 1
         else:
-            lower_row = int(center[1])
+            lower_row = None
 
         for row in [lower_row, int(center[1]), upper_row]:
             for col in [left_col, center_letter_idx, right_col]:
@@ -223,8 +230,6 @@ class ChessVar:
             return False
 
         if type == 'knight':
-            start_letter_idx = columns.find(start[0])
-            finish_letter_idx = columns.find(finish[0])
             if abs(finish_letter_idx - start_letter_idx) == 2:
                 if abs(int(start[1]) - int(finish[1])) == 1:
                     return True
@@ -234,8 +239,6 @@ class ChessVar:
             return False
 
         if type == 'bishop':
-            start_letter_idx = columns.find(start[0])
-            finish_letter_idx = columns.find(finish[0])
             if abs(int(start[1]) - int(finish[1])) == 0:
                 return False
             elif abs(finish_letter_idx - start_letter_idx)/abs(int(start[1]) - int(finish[1])) == 1:
@@ -243,27 +246,23 @@ class ChessVar:
             return False
 
         if type == 'queen':
-            start_letter_idx = columns.find(start[0])
-            finish_letter_idx = columns.find(finish[0])
             if piece.get_color() == 'white':
-                if start[0] == finish[0] and (1 <= int(finish[1]) - int(start[1]) <= 7):
+                if start_letter_idx == finish_letter_idx and (1 <= int(finish[1]) <= 8):
                     return True
-                elif start[1] == finish[1] and (finish[0] in columns):
+                elif int(start[1]) == int(finish[1]) and (finish[0] in columns):
                     return True
                 elif abs(finish_letter_idx - start_letter_idx) / abs(int(start[1]) - int(finish[1])) == 1:
                     return True
             elif piece.get_color() == 'black':
-                if start[0] == finish[0] and (1 <= int(start[1]) - int(finish[1]) <= 7):
+                if start_letter_idx == finish_letter_idx and (1 <= int(finish[1]) <= 8):
                     return True
-                elif start[1] == finish[1] and (finish[0] in columns):
+                elif int(start[1]) == int(finish[1]) and (finish[0] in columns):
                     return True
                 elif abs(finish_letter_idx - start_letter_idx) / abs(int(start[1]) - int(finish[1])) == 1:
                     return True
             return False
 
         if type == 'king':
-            start_letter_idx = columns.find(start[0])
-            finish_letter_idx = columns.find(finish[0])
             if abs(int(start[1]) - int(finish[1])) == 1 and abs(finish_letter_idx - start_letter_idx) == 0:
                 return True
             elif abs(finish_letter_idx - start_letter_idx) == 1 and abs(int(start[1]) - int(finish[1])) == 0:
@@ -275,24 +274,17 @@ class ChessVar:
 
     def print_board(self):
         """ prints the current state of the board to output terminal """
-        board_print = ''
+        board_print = '\n'
         for row in '87654321':
             for col in 'abcdefgh':
                 position = col + row
                 if position in self._board:
                     piece = self._board[position]
-                    if piece.get_color() == 'white':
-                        if piece.get_type() == 'knight':
-                            board_print += 'N'
-                        else:
-                            board_print += piece.get_type()[0].upper()
-                    elif piece.get_color() == 'black':
-                        if piece.get_type() == 'knight':
-                            board_print += 'n'
-                        else:
-                            board_print += piece.get_type()[0]
+                    board_print += piece.get_art()
                 else:
-                    board_print += '*'
+                    board_print += '.'
                 board_print += '  '
             board_print += '\n'
         print(board_print)
+        if self._game_state == 'UNFINISHED':
+            print('Current turn:', self._current_turn, '\n')
